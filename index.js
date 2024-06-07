a14b70aa243e8f41a24b87587dafbf1afff3b66d
HewkawAr <hewkawar@gmail.com>
2024-06-07 23:49:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,51 @@ const { version } = require("./package.json");
 
 require('dotenv').config();
 
-// Sharding Manager
-const manager = new ShardingManager("bot.js", {
-    token: process.env.TOKEN
-});
+const TOKEN = process.env.TOKEN;
+const MAX_SERVERS_PER_SHARD = 10;
 
-manager.on('shardCreate', shard => {
-    shard.on('death', () => {
-        console.log(`[${shard.id}] is Death`);
-        console.log(`[${shard.id}] Reswawning...`)
-    })
-    console.log(`Launched shard ${shard.id}`);
-});
-
-manager.spawn().then(shards => {
-    shards.forEach(shard => {
-        shard.on('message', message => {
-            console.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
+async function getServerCount() {
+    try {
+        const response = await fetch("https://discord.com/api/v10/users/@me/guilds", {
+            headers: {
+                "Authorization": `Bot ${TOKEN}`
+            }
         });
+        return await response.json().length;
+    } catch (error) {
+        console.error("Error fetching server count:", error);
+        return 0;
+    }
+}
+
+async function startSharding() {
+    const serverCount = await getServerCount();
+    const totalShards = Math.ceil(serverCount / MAX_SERVERS_PER_SHARD);
+
+    // Sharding Manager
+    const manager = new ShardingManager("bot.js", {
+        token: TOKEN,
+        totalShards: totalShards
     });
-}).catch(console.error);
+
+    manager.on('shardCreate', shard => {
+        shard.on('death', () => {
+            console.log(`[${shard.id}] is Death`);
+            console.log(`[${shard.id}] Reswawning...`);
+        });
+        console.log(`Launched shard ${shard.id}`);
+    });
+
+    manager.spawn().then(shards => {
+        shards.forEach(shard => {
+            shard.on('message', message => {
+                console.log(`Shard[${shard.id}] : ${message._eval} : ${message._result}`);
+            });
+        });
+    }).catch(console.error);
+}
+
+startSharding();
 
 // API Manager
 const api = express();
