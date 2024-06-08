3c68ffedada17ca6011016048fb0ac236bb207a8
HewkawAr <hewkawar@gmail.com>
2024-06-08 21:08:27 +0700

    Update ping.js

--- commands/Common/ping.js
+++ commands/Common/ping.js
@@ -16,25 +16,11 @@ module.exports = {
      * @param {Locale} locale 
      */
     async execute(interaction, client, locale) {
-        const startTime = Date.now();
-
-        const pingTest = await interaction.reply({
+        await interaction.reply({
             embeds: [
                 new EmbedBuilder()
                     .setColor(Colors.Blue)
-                    .setDescription(locale.getLocaleString("command.ping.pinging"))
-                    .setFooter({ text: locale.replacePlaceholders(locale.getLocaleString("command.footer"), [client.user.displayName, version]) })
-                ]
-        });
-
-        const endTime = Date.now();
-        const ping = endTime - startTime;
-
-        await pingTest.edit({
-            embeds: [
-                new EmbedBuilder()
-                    .setColor(Colors.Blue)
-                    .setDescription(locale.replacePlaceholders(locale.getLocaleString("command.ping.description"), [ping, client.ws.ping]))
+                    .setDescription(locale.replacePlaceholders(locale.getLocaleString("command.ping.description"), [Date.now() - interaction.createdTimestamp, client.ws.ping]))
                     .setFooter({ text: locale.replacePlaceholders(locale.getLocaleString("command.footer"), [client.user.displayName, version]) })
             ]
         });
