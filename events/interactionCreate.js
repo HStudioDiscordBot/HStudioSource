b62d2f9343fc35f117e388437ac9b9d777b2c4ba
HewkawAr <hewkawar@gmail.com>
2024-06-19 18:59:48 +0700

    update Buttons

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,6 +2,7 @@ const { Events, Client, Interaction, Colors, EmbedBuilder, ActionRowBuilder, Tex
 const Locale = require("../class/Locale");
 const donateModal = require("../modals/donate");
 const createAdsModal = require("../modals/create_ads");
+const buttonCreateAds = require("../buttons/buttonCreateAds");
 
 module.exports = {
     name: Events.InteractionCreate,
@@ -56,53 +57,7 @@ module.exports = {
             const customId = interaction.customId;
 
             if (customId == "buttonCreateAds") {
-                const createModal = new ModalBuilder()
-                    .setCustomId("create_ads")
-                    .setTitle(locale.getLocaleString("command.ads.create.modal.title"));
-
-                const createModalDescription = new TextInputBuilder()
-                    .setCustomId("description")
-                    .setLabel(locale.getLocaleString("command.ads.create.modal.description"))
-                    .setRequired(true)
-                    .setStyle(TextInputStyle.Paragraph);
-
-                const createModalImageUrl = new TextInputBuilder()
-                    .setCustomId("imageUrl")
-                    .setLabel(locale.getLocaleString("command.ads.create.modal.imageUrl"))
-                    .setRequired(true)
-                    .setStyle(TextInputStyle.Short);
-
-                const createModalButtonText = new TextInputBuilder()
-                    .setCustomId("buttonText")
-                    .setLabel(locale.getLocaleString("command.ads.create.modal.button.text"))
-                    .setPlaceholder(locale.getLocaleString("command.ads.create.modal.button.text.placeholder"))
-                    .setValue(locale.getLocaleString("command.ads.create.modal.button.text.value"))
-                    .setRequired(true)
-                    .setStyle(TextInputStyle.Short)
-                    .setMaxLength(50);
-
-                const createModalButtonUrl = new TextInputBuilder()
-                    .setCustomId("buttonUrl")
-                    .setLabel(locale.getLocaleString("command.ads.create.modal.button.url"))
-                    .setRequired(true)
-                    .setStyle(TextInputStyle.Short);
-
-                const createModalVoucherUrl = new TextInputBuilder()
-                    .setCustomId("voucherUrl")
-                    .setRequired(true)
-                    .setPlaceholder("https://gift.truemoney.com/campaign/?v=xxxxx")
-                    .setLabel(locale.getLocaleString("command.ads.create.modal.voucher.url"))
-                    .setStyle(TextInputStyle.Short);
-
-                const createDescriptionAction = new ActionRowBuilder().addComponents(createModalDescription);
-                const createImageUrlAction = new ActionRowBuilder().addComponents(createModalImageUrl);
-                const createButtonTextAction = new ActionRowBuilder().addComponents(createModalButtonText);
-                const createButtonUrlAction = new ActionRowBuilder().addComponents(createModalButtonUrl);
-                const createVoucherUrlAction = new ActionRowBuilder().addComponents(createModalVoucherUrl);
-
-                createModal.addComponents(createDescriptionAction, createImageUrlAction, createButtonTextAction, createButtonUrlAction, createVoucherUrlAction);
-
-                await interaction.showModal(createModal);
+                buttonCreateAds.execute(interaction, client, locale);
             }
         }
     }
