5f61e3450d518ae4ff386157cfe87d4060c77f21
HewkawAr <hewkawar@gmail.com>
2024-06-27 18:20:44 +0700

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs"
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  },
   pluginJs.configs.recommended,
 ];
