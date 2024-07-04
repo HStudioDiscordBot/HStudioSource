3d2dbb2e953110aa561821c6f9bfcb38fe426e54
HewkawAr <hewkawar@gmail.com>
2024-07-04 18:10:20 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -14,7 +14,7 @@ module.exports = {
     async execute(client) {
         function setActivity() {
             client.user.setActivity({
-                name: `/help | ${client.guilds.cache.size.toLocaleString()} servers | V${version}`,
+                name: `🔊 /help | ${client.guilds.cache.size.toLocaleString()} servers | V${version}`,
                 type: ActivityType.Custom
             });
         }
