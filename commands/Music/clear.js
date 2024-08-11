fa9082af5f3dfd5734dd3b491d5afe1666087193
HewkawAr <hewkawar@gmail.com>
2024-08-11 09:14:59 +0700

    Update clear.js
      1:52  error  'ActionRowBuilder' is assigned a value but never used  no-unused-vars
      1:70  error  'ButtonBuilder' is assigned a value but never used     no-unused-vars
      1:85  error  'ButtonStyle' is assigned a value but never used       no-unused-vars

--- commands/Music/clear.js
+++ commands/Music/clear.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, Colors, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
